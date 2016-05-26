55e7a110ec3db7b43d28614d6f22d2a537589077
Edgecastle Dev <github@edgecastle.com>
2016-05-26 22:32:58 +0100

    Initial commit

---
+++ Samples/Edgecastle.IdentityServer4.Neo4j.Samples.AspNetCore/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Edgecastle.IdentityServer4.Neo4j.Samples.AspNetCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
